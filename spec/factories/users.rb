471f3ea50e6b5a9c4f91353421b6ee3929354e14
Jimmy Ranger <jranger@me.com>
2016-07-05 16:23:00 -0400

    create devise user model with model tests, create empty UsersController

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  sequence(:email) { |n| "bg#{n}@pipedpiper.net" }
+  factory :user do
+    first_name "Bertram"
+    last_name "Guilfoyle"
+    email
+    profile_picture "photo.png"
+    role "member"
+    password "SatanCanada"
+    password_confirmation "SatanCanada" 
+  end
+end
