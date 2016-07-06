82e5d10f95db52f8b86db8f184736ed9fb831725
Jimmy Ranger <jranger@me.com>
2016-07-06 13:57:52 -0400

    removed belongs_to from spec files

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Review, type: :model do
-  it { should belong_to :user }
-  it { should belong_to :donut }
-
   it { should have_valid(:rating).when(2, 5) }
   it { should_not have_valid(:rating).when(6, 2.3, "") }
 
