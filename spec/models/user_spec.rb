71e2359171b498d70aa50cc0652a40d86b0cad02
Jimmy Ranger <jranger@me.com>
2016-07-06 11:04:11 -0400

    addressed hound violations and review comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_valid(:first_name).when("Jimmy", "Linc")}
-  it { should_not have_valid(:first_name).when(nil, "")}
+  it { should have_valid(:first_name).when("Jimmy", "Linc") }
+  it { should_not have_valid(:first_name).when(nil, "") }
 
-  it { should have_valid(:last_name).when("Ranger", "Kupke")}
-  it { should_not have_valid(:last_name).when(nil, "")}
+  it { should have_valid(:last_name).when("Ranger", "Kupke") }
+  it { should_not have_valid(:last_name).when(nil, "") }
 
-  it { should have_valid(:email).when("example@gmail.com", "erlich@aviato.net")}
-  it { should_not have_valid(:email).when("example", "example@", "exam@gma", "example.com", nil)}
+  it { should have_valid(:email).when("example@gmail.com", "erlich@aviato.net") }
+  it { should_not have_valid(:email).when("example", "example@", "exam@gma", "example.com", "", nil) }
 
   describe "#admin?" do
     context "when user is a admin" do
-      it "should give back true if user an admin" do
+      it "should return true if user is an admin" do
         user = create(:user, role: "admin")
         expect(user.admin?).to eq(true)
       end
     end
+    
     context "when user is a member" do
-      it 'should give back false' do
+      it "should give back false" do
         user = create(:user)
         expect(user.admin?).to eq(false)
       end
@@ -31,5 +32,4 @@ RSpec.describe User, type: :model do
       expect(user.fullname).to eq("Bertram Guilfoyle")
     end
   end
-
 end
