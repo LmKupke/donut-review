e95be3b5f7390ebbbc72bdd2b56063391bdec335
Jimmy Ranger <jranger@me.com>
2016-07-06 13:33:28 -0400

    update review controller, begun review model tests, updated hound.yml and added ruby-config.yml

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @dount = Donut.find(params[:donut_id])
+    @review = Review.new
+  end
+
+  def create
+    @dount = Donut.find(params[:donut_id])
+    @review = Review.new(review_params)
+    @review.donut = @donut
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :body)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
-class Review << ActiveRecord::Base
+class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :donut
 
