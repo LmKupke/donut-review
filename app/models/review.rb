723596fbec560e34de06648d46b2e578b3a2fc3a
Jimmy Ranger <jranger@me.com>
2016-07-06 13:55:34 -0400

    remove belongs_to from review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,4 @@
 class Review < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :donut
-
   validates :user_id, presence: true
   validates :rating, presence: true, numericality: { only_integer: true, minimum: 0, maximum: 5 }
   validates :donut_id, presence: true
