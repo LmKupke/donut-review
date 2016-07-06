51d77d8d551e29c3831b86b7a2fa901db3bdfb9b
Jimmy Ranger <jranger@me.com>
2016-07-06 14:48:13 -0400

    last hound violation fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   validates :user_id, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true}, inclusion: { in: 1..5 }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
   validates :donut_id, presence: true
   validates :body, length: { maximum: 140 }
 end
