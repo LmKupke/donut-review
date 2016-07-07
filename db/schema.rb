d1ac5ae0cf83110ad651da83cf4e54e6099e208c
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-07 16:21:01 -0400

    forgot to migrate before push

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160705194034) do
+ActiveRecord::Schema.define(version: 20160706152154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
