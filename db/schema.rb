dbf4cf54d09810b30c84aa702ddadbf96d29cf5f
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-07 10:02:37 -0400

    forgot to bundle and had to migrate, first time using computer

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705192018) do
+ActiveRecord::Schema.define(version: 20160705194034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "donuts", force: :cascade do |t|
+    t.string  "name",        null: false
+    t.text    "description"
+    t.string  "vendor_name", null: false
+    t.string  "image",       null: false
+    t.integer "user_id",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                                null: false
