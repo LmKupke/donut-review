e1029f6dfae0c574cd21099bd7f727060c25336a
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-08 14:50:00 -0400

    fixed hound violations for array

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,12 +1,9 @@
 class Vendor < ActiveRecord::Base
   has_many :donuts
   STATES = %W(
-    'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL',
-    'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH',
-    'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM',
-    'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC',
-    'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY', 'AE',
-    'AA', 'AP'
+    AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA KS KY LA ME MH
+    MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND MP OH OK OR PW PA PR RI SC
+    SD TN TX UT VT VI VA WA WV WI WY AE AA AP
   )
 
   validates :name, presence: true
