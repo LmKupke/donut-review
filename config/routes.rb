1cfc641e9a62692eb78267fa77807481b5e9ff57
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-08 11:33:34 -0400

    Fixed hound spacing errors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "donuts#index"
   resources :donuts
   devise_for :users, controllers: {
-      registrations: "users/registrations",
-      sessions: "users/sessions"
-    }
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   resources :users, only: [:show, :destroy]
 end
