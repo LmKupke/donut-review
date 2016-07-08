7dd520c4d70b77441c9c3feed17c23cb8b0ab903
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-08 11:31:43 -0400

    Aligned method parameters in config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root "donuts#index"
   resources :donuts
-  devise_for :users,
-    controllers: {
+  devise_for :users, controllers: {
       registrations: "users/registrations",
       sessions: "users/sessions"
     }
