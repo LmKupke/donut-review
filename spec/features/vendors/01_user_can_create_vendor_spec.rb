151873c83763105012de9a197b9949fe39fdc646
Jimmy Ranger <jranger@me.com>
2016-07-11 13:49:30 -0400

    added empty spec files

---
+++ spec/features/vendors/01_user_can_create_vendor_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "user sees a vendor form", %{
+  As a user
+  I wanna see a form for vendors
+  So that I can add a vendor
+} do
+  #ACCEPTANCE CRITERIA:
+  # [] Vendor#create goes to vendors/new
+  # [] 
+end
