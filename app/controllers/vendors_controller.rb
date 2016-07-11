61fb6bed1a6d91ed14a62d256d22a917774780ca
Jimmy Ranger <jranger@me.com>
2016-07-11 14:39:49 -0400

    added link to form, wrote some vendor tests

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -5,6 +5,15 @@ class VendorsController < ApplicationController
 
   def create
     @vendor = Vendor.new(vendor_params)
+    if @vendor.save
+      redirect_to @vendor, notice: "You have successfully created a new vendor!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
   end
 
   private
