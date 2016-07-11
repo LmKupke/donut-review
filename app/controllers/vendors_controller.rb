7dc33ab0e322a6f81b81dff956cb760a0841bfc9
Jimmy Ranger <jranger@me.com>
2016-07-11 13:39:13 -0400

    added vendor form

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,15 @@
 class VendorsController < ApplicationController
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :street_number, :street_name, :city, :state, :zipcode)
+  end
 end
