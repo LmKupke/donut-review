9f9116010004a8ca1e98ee5612b09a23b78e501a
Jimmy Ranger <jranger@me.com>
2016-07-12 10:46:05 -0400

    sets up vote model

---
+++ app/models/vote.rb
@@ -0,0 +1,8 @@
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :review
+
+  validates :user_id, presence: true
+  validates :review_id, presence: true
+  validates :vote, presence: true, inclusion: { in: [1, 0, -1] }
+end
