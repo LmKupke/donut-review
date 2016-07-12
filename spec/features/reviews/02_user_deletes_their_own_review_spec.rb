83e802a933050dfdfbc51412412e141f65c40f91
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-12 12:08:56 -0400

    Add update and delete functionality for reviews

---
+++ spec/features/reviews/02_user_deletes_their_own_review_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+feature "user deletes a review" do
+  let!(:donut) { create(:donut) }
+
+  scenario "user is the review creator" do
+    user = create(:user)
+    login_as(user)
+    visit root_path
+    click_link donut.name
+    choose("5")
+    fill_in(:review_body, with: "wunderbar")
+    click_button "Create Review"
+    click_button "Delete"
+
+    expect(page).to_not have_content("wunderbar")
+  end
+
+  scenario "user is not the review creator" do
+    review = create(:review)
+    user = create(:user)
+    login_as(user)
+    visit root_path
+    first(:link, donut.name).click
+
+    expect(page).to_not have_button("Delete")
+  end
+end
