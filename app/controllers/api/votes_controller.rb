e3761177adf1813dad9d8e0ce0c783a82d5ef851
Jimmy Ranger <jranger@me.com>
2016-07-12 16:22:19 -0400

    adds API functionality

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -1,35 +1,36 @@
-module API
-  class VotesController < ApplicationController
-    before_action :pre_vote
+# module API
+class Api::VotesController < ApiController
+  before_action :pre_vote
 
-    def upvote
-      binding.pry
-      if @value.vote == 1
-        @value.vote -= 1
-      else
-        @value.vote = 1
-      end
-      @value.save
-      @vote_total = Vote.group(:review_id).sum(:vote)
-      render json: @vote_total[@review.id]
+  def upvote
+    binding.pry
+    if @value.vote == 1
+      @value.vote -= 1
+    else
+      @value.vote = 1
     end
+    @value.save
+    @vote_total = Vote.group(:review_id).sum(:vote)
+    render json: @vote_total[@review.id]
+  end
 
-    def downvote
-      if @value.vote == -1
-        @value.vote += 1
-      else
-        @value.vote = -1
-      end
-      @value.save
-      @vote_total = Vote.group(:review_id).sum(:vote)
-      render json: @vote_total[@review.id]
+  def downvote
+    if @value.vote == -1
+      @value.vote += 1
+    else
+      @value.vote = -1
     end
+    @value.save
+    @vote_total = Vote.group(:review_id).sum(:vote)
+    render json: @vote_total[@review.id]
+  end
 
-    protected
+  protected
 
-    def pre_vote
-      @review = Review.find(params[:review_id])
-      @value = Vote.find_or_initialize_by(review: @review, user: current_user)
-    end
+  def pre_vote
+    binding.pry
+    @review = Review.find(params[:review_id])
+    @value = Vote.find_or_initialize_by(review: @review, user: current_user)
   end
 end
+# end
