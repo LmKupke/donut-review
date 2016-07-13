71917bdea658265edb346f9e672a83601b079395
Jimmy Ranger <jranger@me.com>
2016-07-13 14:45:39 -0400

    capybara wekbit working

--- spec/features/votes/01_user_can_upvote_reviews_spec.rb
+++ spec/features/votes/01_user_can_upvote_reviews_spec.rb
@@ -3,13 +3,13 @@ feature "user upvotes donut ratings", %{
   As a user
   I want to upvote and downvote ratings for a donut
   So I can act like I'm on Reddit
-} do
+}, js: true do
   context 'as an unauthenticated user, ' do
     let!(:donut) { create(:donut) }
     let!(:review) { create(:review, donut: donut, user: donut.user) }
     before(:each) do
-      click_link "Glazed"
       visit root_path
+      click_link "Glazed"
     end
 
     scenario 'I visit donut review page' do
@@ -17,14 +17,13 @@ feature "user upvotes donut ratings", %{
       expect(page).to have_button('-1')
     end
 
-    scenario "Shows user error message to sign in before voting", :js => true do
+    scenario "Shows user error message to sign in before voting" do
       click_button('+1')
-
       expect(page).to have_content('0')
     end
   end
 
-  xcontext 'as an authenticated user, ' do
+  context 'as an authenticated user, ' do
     let!(:review) { create(:review) }
     let!(:authenticated_user) { create(:user) }
     before(:each) do
@@ -33,12 +32,12 @@ feature "user upvotes donut ratings", %{
       click_link 'Glazed'
     end
 
-    xscenario 'I visit donut review page' do
+    scenario 'I visit donut review page' do
       expect(page).to have_content('+1')
       expect(page).to have_content('-1')
     end
 
-    xscenario 'I upvote a review' do
+    scenario 'I upvote a review' do
       click_button('+1')
       expect(page).to have_content('1')
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ Capybara::Webkit.configure do |config|
   config.allow_url("fonts.googleapis.com")
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -45,7 +46,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
