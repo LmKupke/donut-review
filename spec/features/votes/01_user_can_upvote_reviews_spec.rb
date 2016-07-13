c67712ff71f75b72e6321073b5112a3fcfdb7d4d
Jimmy Ranger <jranger@me.com>
2016-07-13 10:24:19 -0400

    got capybara webkit installed not working with tests:

--- spec/features/votes/01_user_can_upvote_reviews_spec.rb
+++ spec/features/votes/01_user_can_upvote_reviews_spec.rb
@@ -5,19 +5,19 @@ feature "user upvotes donut ratings", %{
   So I can act like I'm on Reddit
 } do
   context 'as an unauthenticated user, ' do
-    let!(:review) { create(:review) }
+    let!(:donut) { create(:donut) }
+    let!(:review) { create(:review, donut: donut, user: donut.user) }
     before(:each) do
-      visit root_path
       click_link "Glazed"
+      visit root_path
     end
+
     scenario 'I visit donut review page' do
-      # save_and_open_page
       expect(page).to have_button('+1')
       expect(page).to have_button('-1')
     end
 
-    scenario "Shows user error message to sign in before voting" do 
-      save_and_open_page
+    scenario "Shows user error message to sign in before voting", :js => true do
       click_button('+1')
 
       expect(page).to have_content('0')
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+Capybara.javascript_driver = :webkit
+
+Capybara::Webkit.configure do |config|
+  config.allow_url("fonts.googleapis.com")
+end
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
