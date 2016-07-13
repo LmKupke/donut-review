72e542e12cbef2b7c6274cbd13d6e87d2e5da695
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-13 14:02:59 -0400

    Added links to donut and vendor index pages in nav bar with tests and style

---
+++ spec/features/donuts/05_user_can_navigate_to_donuts_page_from_anywhere_spec.rb
@@ -0,0 +1,79 @@
+require "rails_helper"
+
+feature "user can navigate to donuts index", %{
+  As a user
+  I want to be able to navigate to the donuts index from any page
+  To look at delicious donuts
+} do
+
+  let!(:donut) { create(:donut) }
+  let!(:user) { create(:user) }
+
+  context "as an authenticated user" do
+    scenario "I can navigate to the donuts index from an individual donut
+      show page" do
+      login_as(user)
+      visit root_path
+      click_link donut.name
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+
+    scenario "I can navigate to the donuts index from the add new donut
+      page" do
+      login_as(user)
+      visit root_path
+      click_link "Add New Donut"
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+
+    scenario "I can navigate to the donuts index from the add new vendor
+      page" do
+      login_as(user)
+      visit root_path
+      click_link "Add New Vendor"
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+
+    scenario "I can navigate to the donuts index page from the vendors
+      section" do
+      login_as(user)
+      visit root_path
+      click_link "All Vendors"
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+  end
+
+  context "as an unauthenticated user" do
+    scenario "I can navigate to the donuts index from an individual donut
+      show page" do
+      visit root_path
+      click_link donut.name
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+
+    scenario "I can navigate to the donuts index page from the vendors
+      section" do
+      visit root_path
+      click_link "All Vendors"
+      click_link "Donuts"
+
+      expect(page).to have_content("Donut Judge Me")
+      expect(page).to have_content(donut.name)
+    end
+  end
+end
