25b91286bdfc1d924643e35b0ca7dd1fff017079
Keane Johnson <keanejohnson2013@gmail.com>
2016-07-14 16:26:00 -0400

    fixed hound errors

--- spec/factories/donuts.rb
+++ spec/factories/donuts.rb
@@ -2,7 +2,9 @@ FactoryGirl.define do
   factory :donut do
     name "Glazed"
     description "A delicious donut"
-    image { File.open(File.join("#{Rails.root}/spec/support/images/photo.png")) }
+    image {
+      File.open(File.join("#{Rails.root}/spec/support/images/photo.png"))
+    }
     user
     vendor
   end
--- spec/models/donut_spec.rb
+++ spec/models/donut_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Donut, type: :model do
   it { should have_valid(:vendor_id).when(2) }
   it { should_not have_valid(:vendor_id).when("", nil) }
 
-  it { should have_valid(:image).when(File.open(File.join("#{Rails.root}/spec/support/images/photo.png"))) }
+  it { should have_valid(:image).when(File.open(File.join(
+    "#{Rails.root}/spec/support/images/photo.png"))) }
   it { should_not have_valid(:image).when("", nil) }
 
   it { should have_valid(:description).when("Great donut") }
