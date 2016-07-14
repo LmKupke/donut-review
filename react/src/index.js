4d6b2af7956c18eceacc369f05647a4d0486fe8f
Jimmy Ranger <jranger@me.com>
2016-07-14 09:14:24 -0400

    sets up react environment

---
+++ react/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Review from 'review';
+
+
+export default Index;
--- react/src/main.js
+++ react/src/main.js
@@ -1,3 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Index from './index';
+
+$(function() {
+  ReactDOM.render(
+    <Index />,
+    document.getElementById('app')
+  );
+});
